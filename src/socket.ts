116600ba1a1490c82a2233157bca1551a491e630
Nguyen Trung Son <nguyentrungson267@gmail.com>
2025-07-17 05:57:29 +0700

    Fix deploy related bug

--- src/socket.ts
+++ src/socket.ts
@@ -10,7 +10,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 export const initSocket = (httpServer: HttpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:5173",
+      origin: process.env.CLIENT_URL?.replace(/\/$/, "") || "http://localhost:5173",
       methods: ["GET", "POST"],
     },
   });
